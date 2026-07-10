4d25dd406fd911bb2fa1cbce6a4a1d9831eff0e9
hansb123 <gksajaj@gmail.com>
2026-07-10 10:32:11 +0900

    first

---
+++ Assets/01.Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+
+
+
+}
